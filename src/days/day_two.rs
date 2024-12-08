f9cceb4446afa49f8c9c1024a372cf4df85eaa4a
godsiom <btking1@my.waketech.edu>
2024-12-08 13:51:52 -0500

    complete day 2 pt 1

--- src/days/day_two.rs
+++ src/days/day_two.rs
@@ -1,65 +1,90 @@
-use std::fs::read_to_string;
+use std::{fs::read_to_string, ops::Index};
 
 pub fn p1() {
     let reports_file = read_to_string("path").expect("failed to read file");
 
-    let reports = reports_file.lines();
+    let reports = reports_file
+        .lines()
+        .into_iter()
+        .map(|report| {
+            report
+                .split(" ")
+                .into_iter()
+                .map(|level| level.parse::<u16>().unwrap())
+                .collect::<Vec<u16>>()
+        })
+        .collect::<Vec<_>>();
 
-    let mut report_count = 0;
+    let dec_reports = reports
+        .clone()
+        .into_iter()
+        .enumerate()
+        .filter(|(_, levels)| {
+            levels
+                .clone()
+                .into_iter()
+                .enumerate()
+                .map(|(i, level)| -> std::cmp::Ordering {
+                    if i != 0 {
+                        level.cmp(&levels[i - 1])
+                    } else {
+                        levels[i + 1].cmp(&level)
+                    }
+                })
+                .all(|level| level == std::cmp::Ordering::Greater)
+        })
+        .collect::<Vec<(usize, Vec<u16>)>>();
 
-    let mut filtered_reports = Vec::new();
-    for report in reports.into_iter() {
-        let f_report: Vec<&str> = report.split(char::is_whitespace).collect();
+    let inc_reports = reports
+        .into_iter()
+        .enumerate()
+        .filter(|(_, levels)| {
+            levels
+                .clone()
+                .into_iter()
+                .enumerate()
+                .map(|(i, level)| -> std::cmp::Ordering {
+                    if i != 0 {
+                        level.cmp(&levels[i - 1])
+                    } else {
+                        levels[i + 1].cmp(&level)
+                    }
+                })
+                .all(|level| level == std::cmp::Ordering::Less)
+        })
+        .collect::<Vec<(usize, Vec<u16>)>>();
+    let mut inc_or_dec = Vec::new();
 
-        filtered_reports.push(f_report);
+    for report in inc_reports.clone().into_iter() {
+        inc_or_dec.push(report.1);
+    }
+    for report in dec_reports.clone().into_iter() {
+        inc_or_dec.push(report.1);
     }
 
-    let mut f_report = (&filtered_reports).into_iter();
+    /*
+    for report in stable_reports(inc_or_dec).into_iter() {
+            println!("{:?}", report.1)
+        }
+    */
 
-    while let Some(levels) = f_report.next() {
-        report_count += 1;
-        let mut inc_counter = 0;
-        let mut dec_counter = 0;
+    println!("{}", stable_reports(inc_or_dec).len());
+}
 
-        levels.into_iter().enumerate().for_each(|(pos, level)| {
-            if pos != levels.len() - 1 {
-                let curr: usize = levels[pos].parse().expect("parse error");
-
-                let next: usize = levels[pos + 1].parse().expect("parse error");
-                //println!("{report_count} | length {}, next {}", levels.len(), pos + 1);
-                if next > curr {
-                    inc_counter += 1;
-                    println!("INC | {report_count} | current: {curr} next: {next}");
+fn stable_reports(reports: Vec<Vec<u16>>) -> Vec<(usize, Vec<u16>)> {
+    reports
+        .into_iter()
+        .enumerate()
+        .filter(|(_, levels)| {
+            levels.clone().into_iter().enumerate().all(|(j, level)| {
+                if j != 0 {
+                    let next = levels.get(j - 1).unwrap();
+                    let diff = level.abs_diff(*next);
+                    diff >= 1 && diff <= 3
                 } else {
-                    dec_counter += 1;
-                    println!("DEC | {report_count} | current: {curr} next: {next}");
+                    j == 0
                 }
-            } else {
-                let curr: usize = levels[pos].parse().expect("parse error");
-
-                let prev: usize = levels[pos - 1].parse().expect("parse error");
-
-                if curr > prev {
-                    inc_counter += 1;
-
-                    println!(
-                        "INC | {report_count} | current: {curr} prev
-: {prev}"
-                    );
-                } else {
-                    dec_counter += 1;
-
-                    println!("DEC | {report_count} | current: {curr} prev: {prev}");
-                }
-            }
-        });
-
-        println!(
-            "\n{} | inc: {} dec: {} length: {}\n",
-            report_count,
-            inc_counter,
-            dec_counter,
-            levels.len()
-        );
-    }
+            })
+        })
+        .collect::<Vec<_>>()
 }
